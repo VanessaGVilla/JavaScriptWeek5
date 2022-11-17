b92f51174686f0c5d563ff5b44356a65f0e79c90
Vanessa Garcia <nessa619@msn.com>
2022-11-16 20:24:03 -0800

    added pop statement

---
+++ script.js
@@ -0,0 +1,29 @@
+let myGrades = [ 95, 100, 76, 100, 85 ];
+console.log(myGrades[2]); // 76
+console.log(myGrades[0]); // 95
+console.log(myGrades.length); // 5
+
+myGrades.push(150);
+console.log(myGrades);//
+
+myGrades.push(200);
+console.log(myGrades);//
+
+myGrades.push(300);
+console.log(myGrades);//
+
+myGrades.push(400);
+console.log(myGrades);//
+
+myGrades.push(800);
+console.log(myGrades);//
+
+console.log(myGrades.pop());// 800
+console.log(myGrades.pop());// 300
+console.log(myGrades.pop());// 200
+
+
+
+
+
+
